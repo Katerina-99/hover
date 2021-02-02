925af38064265966da665b149989936cee88246f
Katerina-99 <57578765+Katerina-99@users.noreply.github.com>
2021-02-02 18:40:49 -0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $(document).ready(function(){
   $('.card-1').click(function () {
     // console.log('клик')
-  $('.title-1').toggleClass('orange')
+    $('.title-1').toggleClass('orange');
+    $('.title-2').removeClass('orange')
   });
   $('.card-2').click(function () {
     // console.log('клик')
-  $('.title-2').toggleClass('orange')
+    $('.title-2').toggleClass('orange');
+    $('.title-1').removeClass('orange')
   });
   $('.card').mouseleave(function(){
     // console.log('work')
